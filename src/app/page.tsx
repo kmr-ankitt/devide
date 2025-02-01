8eace4e7c6faeaabcf7494f68f01b9190b99b771
ankit <sahuankit.murhu@gmail.com>
2025-02-01 16:03:26 +0530

    create user and secure auth done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
+import { SignOutButton, SignUpButton } from "@clerk/nextjs";
+
 export default function Home() {
   return <div>
     
+      <SignUpButton>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Sign up
+        </button>
+      </SignUpButton>
+      <SignOutButton>
+        <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+          Sign out
+        </button>
+      </SignOutButton>
   </div>;
 }
