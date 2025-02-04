1d015cdc87ee5fcbdec0830acbc0caf93e2e90a7
ankit <sahuankit.murhu@gmail.com>
2025-02-04 21:30:07 +0530

    Snippet dialog and save snippets to db

--- src/app/(root)/_components/EditorPanel.tsx
+++ src/app/(root)/_components/EditorPanel.tsx
@@ -10,6 +10,7 @@ import { motion } from "framer-motion";
 import useMounted from "@/hooks/useMounted";
 import { useClerk } from "@clerk/nextjs";
 import { EditorPanelSkeleton } from "./EditorPanelSkeleton";
+import ShareSnippetDialog from "./ShareSnippetDialog";
 
 export default function EditorPanel() {
   const [isShareDialogOpen, setIsShareDialogOpen] = useState(false);
@@ -155,9 +156,9 @@ export default function EditorPanel() {
           {!clerk.loaded && <EditorPanelSkeleton />}
         </div>
       </div>
-      {/* {isShareDialogOpen && (
+      {isShareDialogOpen && (
         <ShareSnippetDialog onClose={() => setIsShareDialogOpen(false)} />
-      )} */}
+      )}
     </div>
   );
 }
