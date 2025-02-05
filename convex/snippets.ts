1d237b24589106f28e3c4d7ec527491de331ec3c
ankit <sahuankit.murhu@gmail.com>
2025-02-05 19:38:46 +0530

    Snippets share page done

--- convex/snippets.ts
+++ convex/snippets.ts
@@ -117,6 +117,35 @@ export const getSnippets = query({
   }
 })
 
+export const getSnippetById = query({
+  args: {
+    snippetId: v.id("snippets")
+  },
+  handler: async (ctx, args) => {
+    const snippet = await ctx.db.get(args.snippetId);
+    if (!snippet) {
+      throw new Error("Snippet not found");
+    }
+    return snippet;
+  }
+})
+
+export const getComments = query({
+  args: {
+    snippetId: v.id("snippets")
+  },
+  handler: async (ctx, args) => {
+    const comments = await ctx.db
+      .query("snippetComments")
+      .withIndex("by_snippet_id")
+      .filter((q) => q.eq(q.field("snippetId"), args.snippetId))
+      .order("desc")
+      .collect();
+
+    return comments;
+  }
+})
+
 export const isSnippetStarred = query({
   args: {
     snippetId: v.id("snippets")
